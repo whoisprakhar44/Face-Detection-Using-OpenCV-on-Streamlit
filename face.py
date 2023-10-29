f3e7670240f43609168b7eb62e1769f1a56b5f46
Prakhar <96052307+whoisprakhar44@users.noreply.github.com>
2023-10-29 17:25:50 +0530

    simple code without streamlit

---
+++ face.py
@@ -0,0 +1,25 @@
+import cv2
+import numpy as np
+
+harcascade = "model/haarcascade_frontalface_default.xml"
+
+cap = cv2.VideoCapture(0)
+
+cap.set(3, 640) #width
+cap.set(4, 480) #height
+
+while True:
+    success, img = cap.read()
+
+    facecascade = cv2.CascadeClassifier(harcascade)
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    faces = facecascade.detectMultiScale(img_gray, 1.1, 4)
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+    cv2.imshow("Face", img)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
